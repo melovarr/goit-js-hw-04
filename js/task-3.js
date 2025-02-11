f5e8d4121f7e7ec7df7cb864c5d1721270046739
melovarr <meeelnik@gmail.com>
2025-02-11 11:19:32 +0200

    did homework 4

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,20 @@
-function filterArray(numbers, value) {
-    const filterNumbers = [];
-    for (let index = 0; index < numbers.length; index++) {
-        if (numbers[index] > value) {
-            filterNumbers.push(numbers[index]);
-        }
-    }
-    return filterNumbers;
-}
+const profile = { username: "Jacob", playTime: 300,
+changeUsername(newName) {
+    profile.username = newName;
+},
+updatePlayTime(hours) {
+    profile.playTime += hours;
+},
+getInfo() {
+    return `${profile.username} has ${profile.playTime} active hours!`;
+    },
+};
 
 
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
